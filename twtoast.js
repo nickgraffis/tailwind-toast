5cb03267f0b20b1709f1d33a82edfe16ca532384
Muhammad Saim <m.saim@eximioussol.com>
2021-04-13 23:54:58 +0500

    fix positionX and positionY problem in toasts and snackbar

--- twtoast.js
+++ twtoast.js
@@ -1,42 +1,52 @@
-const config = require('./twtoast.config.js')
-const Toast = require('./classes/Toast')
-const Snackbar = require('./classes/Snackbar')
+const config = require("./twtoast.config.js");
+const Toast = require("./classes/Toast");
+const Snackbar = require("./classes/Snackbar");
 
 if (config.methods) {
   config.methods.forEach((method) => {
-    eval('Toast.prototype.' + Object.keys(method)[0] + ' = ' + Object.values(method))
-    eval('Snackbar.prototype.' + Object.keys(method)[0] + ' = ' + Object.values(method))
-  })
+    eval(
+      "Toast.prototype." +
+        Object.keys(method)[0] +
+        " = " +
+        Object.values(method)
+    );
+    eval(
+      "Snackbar.prototype." +
+        Object.keys(method)[0] +
+        " = " +
+        Object.values(method)
+    );
+  });
 }
 
 module.exports = {
   toast: () => {
     return new Toast(
-      config.color ? config.color : 'blue',
-      config.icon ? config.icon : 'fas fa-bell',
+      config.color ? config.color : "blue",
+      config.icon ? config.icon : "fas fa-bell",
       config.duration ? config.duration : 3000,
-      config.positionX ? config.positionX : 'center',
-      config.positionY ? config.positionY : 'top',
-      config.fontColor ? config.fontColor : 'grey',
+      config.positionX ? config.positionX : "center",
+      config.positionY ? config.positionY : "top",
+      config.fontColor ? config.fontColor : "grey",
       config.fontTone ? config.fontTone : 100,
       config.tone ? config.tone : 500,
-      config.shape ? config.shape : 'square',
+      config.shape ? config.shape : "square",
       config.speed ? config.speed : 500
-    )
+    );
   },
 
   snackbar: () => {
     return new Snackbar(
-      config.color ? config.color : 'blue',
-      config.icon ? config.icon : 'fas fa-bell',
+      config.color ? config.color : "blue",
+      config.icon ? config.icon : "fas fa-bell",
       config.duration ? config.duration : 3000,
-      config.positionX ? config.positionX : 'center',
-      config.positionY ? config.positionY : 'top',
-      config.fontColor ? config.fontColor : 'grey',
+      config.positionX ? config.positionX : "center",
+      config.positionY ? config.positionY : "top",
+      config.fontColor ? config.fontColor : "grey",
       config.fontTone ? config.fontTone : 100,
       config.tone ? config.tone : 500,
-      config.shape ? config.shape : 'square',
+      config.shape ? config.shape : "square",
       config.speed ? config.speed : 500
-    )
-  }
-}
+    );
+  },
+};
